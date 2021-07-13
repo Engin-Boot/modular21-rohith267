4160fae9c12daf1352560b462fb0d933637b1449
rohith267 <45604487+rohith267@users.noreply.github.com>
2021-07-13 19:16:23 +0530

    Add files via upload

---
+++ TelCo.ColorCoder/Tester.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace ColorCoderProject
+
+{
+
+    class Tester
+    {
+                private static void Main(string[] args)
+        {
+            int pairNumber = 4;
+            ColorMapper.ColorPair testPair1 = PairNumberDecoder.GetColorFromPairNumber(pairNumber);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
+            Debug.Assert(testPair1.majorColor == Color.White);
+            Debug.Assert(testPair1.minorColor == Color.Brown);
+
+            pairNumber = 5;
+            testPair1 = PairNumberDecoder.GetColorFromPairNumber(pairNumber);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
+            Debug.Assert(testPair1.majorColor == Color.White);
+            Debug.Assert(testPair1.minorColor == Color.SlateGray);
+
+            pairNumber = 23;
+            testPair1 = PairNumberDecoder.GetColorFromPairNumber(pairNumber);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
+            Debug.Assert(testPair1.majorColor == Color.Violet);
+            Debug.Assert(testPair1.minorColor == Color.Green);
+
+            ColorMapper.ColorPair testPair2 = new ColorMapper.ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
+            pairNumber = ColorPairEncoder.GetPairNumberFromColor(testPair2);
+            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
+            Debug.Assert(pairNumber == 18);
+
+            testPair2 = new ColorMapper.ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
+            pairNumber = ColorPairEncoder.GetPairNumberFromColor(testPair2);
+            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
+            Debug.Assert(pairNumber == 6);
+        }
+    }
+}
---
+++ TelCo.ColorCoder/colorManual.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+
+namespace ColorCoderProject
+{
+    class colorManual
+    {
+
+        public static void manual()
+        {
+
+            int pairNumber = 1;
+            for (int majorColorNumber = 0; majorColorNumber < 5; majorColorNumber++)
+            {
+                for (int minorColorNumber = 0; minorColorNumber < 5; minorColorNumber++)
+                {
+
+                    Console.WriteLine("[In]Pair Number: {0},Colors: majorColor {1} - minorColor {2}\n", pairNumber, ColorMapper.colorMapMajor[majorColorNumber], ColorMapper.colorMapMinor[minorColorNumber]);
+                    pairNumber++;
+                }
+            }
+
+        }
+
+    }
+}
